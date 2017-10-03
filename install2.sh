6f418a1399b6eaeab33469059ba7a89fb94f9d5c
gonxromero <gonzalo.ron@hotmail.com>
2017-10-03 20:20:22 +0200

    segunda prueba

--- install2.sh
+++ install2.sh
@@ -23,3 +23,5 @@ wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add 
 echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
 sudo apt-get update
 sudo apt-get -y install elasticsearch
+
+echo "fin"
